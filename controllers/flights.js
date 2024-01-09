3ab47d6dbe1bb257886874bb1f2364a5203ce9b7
stuartemery96 <stuemery96@gmail.com>
2024-01-08 18:37:22 -0800

    prefill date

--- controllers/flights.js
+++ controllers/flights.js
@@ -12,7 +12,12 @@ async function index(req, res) {
 }
 
 function newFlight(req, res) {
-  res.render('flights/new', { title: 'Add a New Flight', errorMsg: '' });
+  const newFlight = new Flight();
+  // Obtain the default date
+  const dt = newFlight.departs;
+  let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+  departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+  res.render('flights/new', { departsDate, title: 'Add a New Flight', errorMsg: '' });
 }
 
 async function create(req, res) {
