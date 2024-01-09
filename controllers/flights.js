44d1ee25a9322bd9bcae2af22b95ba9961c2435c
stuartemery96 <stuemery96@gmail.com>
2024-01-08 18:07:42 -0800

    initial commit

---
+++ controllers/flights.js
@@ -0,0 +1,29 @@
+const Flight = require('../models/flight');
+
+module.exports = {
+  index,
+  new: newFlight,
+  create,
+}
+
+async function index(req, res) {
+  const flights = await Flight.find({});
+  res.render('flights/index', { title: 'Flight List', flights });
+}
+
+function newFlight(req, res) {
+  res.render('flights/new', { title: 'Add a New Flight', errorMsg: '' });
+}
+
+async function create(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+  }
+  try {
+    await Flight.create(req.body);
+    res.redirect('/flights/new');
+  } catch (err) {
+    console.log(err);
+    res.render('flights/new', { errorMsg: error.message });
+  }
+}
---
+++ routes/flights.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const flightsCtrl = require('../controllers/flights');
+
+// All paths start with "/flights"
+
+// Get /flights (index Action)
+router.get('/', flightsCtrl.index);
+// Get /flights/new (new Action)
+router.get('/new', flightsCtrl.new);
+// Post /flights (create Action)
+router.post('/', flightsCtrl.create);
+
+module.exports = router;
